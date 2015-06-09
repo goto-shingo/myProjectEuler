dd0ea17faed2a1d62eb1c807b31cb398759f2b8c
Shingo goto <gshingo1@gmail.com>
2015-06-10 06:41:36 +0900

    problem001 was completed.

---
+++ problem001.scala
@@ -0,0 +1,11 @@
+/*
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+object problem001 {
+  def main(args: Array[String]) {
+    var answer = 0
+    answer = (1 until 1000).toList.filter(n => n % 3 == 0 || n % 5 == 0).sum
+    println("answer is " + answer)
+  }
+}
